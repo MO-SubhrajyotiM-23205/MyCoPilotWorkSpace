17e9b51d091d21b28364ab860a13db6c07400f12
MO-SubhrajyotiM-23205 <Subhrajyoti.mukhopadhyay@motilaloswal.com>
2025-08-19 18:24:52 +0530

    country drp

--- taskManagementSys/src/App.jsx
+++ taskManagementSys/src/App.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import ListTest from './components/listTest';
 
 const App =()=>{
 
+  // return (
+  //   <div className='app_container'>
+  //     <TaskForm />
+  //     <main className='app_main'>
+  //       <section className='app_main_section'>Section 1</section>
+  //       <section className='app_main_section'>Section 2</section>
+  //       <section className='app_main_section'>Section 3</section>
+  //     </main>
+      
+  //   </div>
+  // )
+  const [selectedCountry, setSelectedCountry] = React.useState("");
   return (
-    <div className='app_container'>
-      <header className='app_header'> Task Management System</header>
-      <main className='app_main'>
-        <section className='app_main_section'>Section 1</section>
-        <section className='app_main_section'>Section 2</section>
-        <section className='app_main_section'>Section 3</section>
-      </main>
+    <div>
+      <label htmlFor="country-dropdown">Country: </label>
+      <ListTest
+        asDropdown={true}
+        value={selectedCountry}
+        onChange={e => setSelectedCountry(e.target.value)}
+      />
+      {selectedCountry && (
+        <div style={{marginTop: '1rem'}}>Selected Country: {selectedCountry}</div>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ taskManagementSys/src/components/TaskForm.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const TaskForm = () => {
+    return (
+        <header className='app_header'> Task Management System</header>
+    );
+};
+
+export default TaskForm;
