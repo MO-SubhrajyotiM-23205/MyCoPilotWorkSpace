4249570de59a64ace816cde91c935fea35191c47
MO-SubhrajyotiM-23205 <Subhrajyoti.mukhopadhyay@motilaloswal.com>
2025-08-19 15:40:55 +0530

    design change

--- taskManagementSys/src/App.jsx
+++ taskManagementSys/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import './App.css';
 
 const App =()=>{
 
   return (
-    <>
-      <h1>Task Management System</h1>
-    </>
+    <div className='app_container'>
+      <header className='app_header'> Task Management System</header>
+      <main className='app_main'>
+        <section className='app_main_section'>Section 1</section>
+        <section className='app_main_section'>Section 2</section>
+        <section className='app_main_section'>Section 3</section>
+      </main>
+    </div>
   )
 }
 
