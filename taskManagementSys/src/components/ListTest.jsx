02af1f70f7d06df96779384e5985b27178f47bdd
MO-SubhrajyotiM-23205 <Subhrajyoti.mukhopadhyay@motilaloswal.com>
2025-08-20 10:27:36 +0530

    comment Dropdown section

--- taskManagementSys/src/components/ListTest.jsx
+++ taskManagementSys/src/components/ListTest.jsx
@@ -4,27 +4,21 @@ import React from "react";
 
 const countries = ["India", "USA", "Canada"];
 
-const ListTest = ({ asDropdown = false, onChange, value }) => {
-    if (asDropdown) {
-        return (
-            <select value={value} onChange={onChange}>
-                <option value="">Select Country</option>
-                {countries.map((country) => (
-                    <option key={country} value={country}>{country}</option>
-                ))}
-            </select>
-        );
-    }
-    return (
-        <div>
-            <ul>
-                {countries.map((country) => (
-                    <li key={country}>{country}</li>
-                ))}
-            </ul>
-        </div>
+const ListTest = ({ asDropdown = false, onChange, value }) =>
+    asDropdown ? (
+        <select value={value} onChange={onChange}>
+            <option value="">Select Country</option>
+            {countries.map(country => (
+                <option key={country} value={country}>{country}</option>
+            ))}
+        </select>
+    ) : (
+        <ul>
+            {countries.map(country => (
+                <li key={country}>{country}</li>
+            ))}
+        </ul>
     );
-};
 
 
 
