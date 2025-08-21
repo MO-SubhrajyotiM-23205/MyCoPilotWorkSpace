0203b6003d9a18c348da02b650e719a9d2df216e
MO-SubhrajyotiM-23205 <Subhrajyoti.mukhopadhyay@motilaloswal.com>
2025-08-21 19:12:21 +0530

    tagand Task form

---
+++ taskManagementSys/src/components/TagButton.jsx
@@ -0,0 +1,11 @@
+
+
+const TagButton = ({ Taglabel }) => {
+    return (
+        <button className='tag'>
+            {Taglabel}+
+        </button>
+    );
+};
+
+export default TagButton;
--- taskManagementSys/src/components/TaskForm.jsx
+++ taskManagementSys/src/components/TaskForm.jsx
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
+import './TaskForm.css'; // Assuming you have a CSS file for styling
+import TagButton from './TagButton';
+ // Adjust the path as necessary
 
 const TaskForm = () => {
+
     return (
-        <header className='app_header'> Task Management System</header>
+        <div>
+            <header className='app_header'> Task Management System</header>
+            <form>
+                
+                <input type="text" className='task_input' placeholder="Task Title" />
+                <div className='task_form_actions'>
+                    <TagButton Taglabel="HTML" />
+                    <TagButton Taglabel="CSS" />
+                    <TagButton Taglabel="JavaScript" />
+                    <TagButton Taglabel="React" />
+
+                    <select className='task_status'>
+                        <option value="todo">To Do</option>
+                        <option value="in-progress">In Progress</option>
+                        <option value="done">Done</option>
+                    </select>
+                    <button type='submit' className='task_submit'>Add Task</button>
+                   
+                </div>
+            </form>
+        </div>
     );
 };
 
