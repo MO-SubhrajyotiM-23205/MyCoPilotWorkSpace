904ffd7017ccff78ac0eb822b359928e7ed8a44f
MO-SubhrajyotiM-23205 <Subhrajyoti.mukhopadhyay@motilaloswal.com>
2025-08-22 18:16:00 +0530

    form functionality

--- taskManagementSys/src/components/TaskForm.jsx
+++ taskManagementSys/src/components/TaskForm.jsx
@@ -1,33 +1,53 @@
 import React, { useState } from 'react';
 import './TaskForm.css'; // Assuming you have a CSS file for styling
 import TagButton from './TagButton';
- // Adjust the path as necessary
+// Adjust the path as necessary
 
 const TaskForm = () => {
 
-    return (
-        <div>
-            <header className='app_header'> Task Management System</header>
-            <form className='task_form'>
+    const [formData, setFormData] = useState({
+        title: "",
+        status: "todo",
+        tags: []
+    });
 
-                <input type="text" className='task_input' placeholder="Task Title" />
-                <div className='task_form_actions'>
-                    <TagButton Taglabel="HTML" />
-                    <TagButton Taglabel="CSS" />
-                    <TagButton Taglabel="JavaScript" />
-                    <TagButton Taglabel="React" />
 
-                    <select className='task_status'>
-                        <option value="todo">To Do</option>
-                        <option value="in-progress">In Progress</option>
-                        <option value="done">Done</option>
-                    </select>
-                    <button type='submit' className='task_submit'>Add Task</button>
-                   
-                </div>
-            </form>
-        </div>
-    );
+const setInputData = (e) => {
+    const { name, value } = e.target;
+    // setFormData({
+    //     ...formData,
+    //     [name]: value,[status]: value
+    // });
+    setFormData(prevData => ({
+        ...prevData,
+        [name]: value
+    }))
+};
+console.log(formData);
+
+return (
+    <div>
+        <header className='app_header'> Task Management System</header>
+        <form className='task_form'>
+
+            <input type="text" name="title" className='task_input' placeholder="Task Title" onChange={setInputData} />
+            <div className='task_form_actions' >
+                <TagButton Taglabel="HTML" />
+                <TagButton Taglabel="CSS" />
+                <TagButton Taglabel="JavaScript" />
+                <TagButton Taglabel="React" />
+
+                <select name="status" className='task_status' onChange={setInputData}>
+                    <option value="todo">To Do</option>
+                    <option value="in-progress">In Progress</option>
+                    <option value="done">Done</option>
+                </select>
+                <button type='submit' className='task_submit'>Add Task</button>
+
+            </div>
+        </form>
+    </div>
+);
 };
 
 export default TaskForm;
