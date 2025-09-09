68d49a31d6fe39276faf63f937e7e1d034719fef
MO-SubhrajyotiM-23205 <Subhrajyoti.mukhopadhyay@motilaloswal.com>
2025-09-09 14:29:40 +0530

    Some Changes

--- MovieManiac/src/assets/Components/Movielist/APIcallthroughUseData.jsx
+++ MovieManiac/src/assets/Components/Movielist/APIcallthroughUseData.jsx
@@ -3,17 +3,26 @@ import useData from "../../../hooks/useData";
 
 
 const APIcallthroughUseData = () => {
-  const { data, error, loading } = useData("http://localhost:5000/api/data");
+  const { data, error, loading } = useData(
+            "/api?LATITUDE=32.7221248&LONGITUDE=74.858496",
+            {
+                method: "POST",
+                headers: {
+                    "x-api-key": "YGjtkqG4l17CDyC29MHTx7OiG865pA3taHbelODV",
+                    "authorizationToken": "tyK3DkD00111USXT"
+                },
+                body: JSON.stringify({ LATITUDE: 32.7221248, LONGITUDE: 74.858496 })
+            }
+        );
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
-  return (
-    <div>
-      <h1>Data from API:</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+        return (
+            <div>
+                <h2>API Test</h2>
+                {loading && <div>Loading...</div>}
+                {error && <div style={{ color: "red" }}>Error: {error}</div>}
+                {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+            </div>
+        );
 };
 
 export default APIcallthroughUseData;
